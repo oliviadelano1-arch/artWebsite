a6edd76d373bafbe1cac02800f811c6648a5b228
oliviadelano1-arch <olivia.delano1@gmail.com>
2026-05-09 15:59:17 -0400

    first commit

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
     "shadcn/ui compatible react aria components that you can copy and paste into your apps. Accessible. Customizable. Open Source. Formerly known as shadcn-aria.",
   links: {
     
-    instagram:""
+    instagram:"https://www.instagram.com/olivepaintings?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
   },
 }
 
